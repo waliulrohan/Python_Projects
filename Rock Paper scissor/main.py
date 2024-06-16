ead846b7c8c9ca3552a3b501d88083497a790c12
waliulohan <waliulislam1742@gmail.com>
2024-06-16 12:58:06 +0600

    rock paper scissors game

---
+++ Rock Paper scissor/main.py
@@ -0,0 +1,32 @@
+import random
+
+'''
+0 for rock
+1 for paper
+2 for scissors
+'''
+my_list = ['rock' , 'paper' , 'scissors']
+
+print(' \n 0 for rock, 1 for paper, 2 for scissors')
+while True:
+    user_input = input("\n \n Choose (or type 'exit' to stop) :")
+
+    if user_input == "exit":
+      print("Thanks for playing.Goodbye.")
+      break
+
+    if user_input in ["1" ,"2","0"]:
+        user = int(user_input)
+        print(f"you choose: {my_list[user]}")
+        comp = random.choice([0 ,1 ,2])
+        print(f'Computer choose: {my_list[comp]}')
+
+        
+        if user == comp:
+            print("It's a DRAW")
+        elif (user == 0 and comp == 2) or (user == 1 and comp == 0) or (user == 2 and comp == 1):
+            print("YOU WON")
+        else:
+            print("YOU LOST")
+    else:
+        print("Invalid input. Please enter 0 for rock, 1 for paper, or 2 for scissors.")
